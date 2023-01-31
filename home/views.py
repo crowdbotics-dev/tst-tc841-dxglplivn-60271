0d17712ec70afd5d5fc0fb53b574ac352a58ad5e
Crowdbotics <engineering@crowdbotics.com>
2023-01-31 03:32:13 +0000

    add package blazer

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'blazer', 'url': 'https://rubygems.org/gems/blazer'},
 	{'name':'django-allauth', 'url': 'https://pypi.org/project/django-allauth/0.38.0/'},
 	{'name':'django-bootstrap4', 'url': 'https://pypi.org/project/django-bootstrap4/0.0.7/'},
 	{'name':'djangorestframework', 'url': 'https://pypi.org/project/djangorestframework/3.9.0/'},
